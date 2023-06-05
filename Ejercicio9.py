56a9dc28267c84a51becc3cab1001a381b20a5b3
Carolina Martinez <carolinamtz091@gmail.com>
2023-06-04 22:25:12 -0600

    primera version

---
+++ Ejercicio9.py
@@ -0,0 +1,10 @@
+#llenar una lista con los números del 1 al 50 y mostrarlos
+
+#Agregamos a la lista los elementos del 1 al 50
+lista = list(range(1,51))
+
+#Imprimimos los elementos de la lista
+for i in lista:
+    print(i,end="-")
+
+#Carolina EM
